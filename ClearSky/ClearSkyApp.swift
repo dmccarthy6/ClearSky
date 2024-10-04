f601390e5e803f34f60fba2728b363ccf975c4b7
Dylan McCarthy <mccarthy.dylan@gmail.com>
2024-10-04 10:22:34 -0400

    Initial Commit

---
+++ ClearSky/ClearSkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearSkyApp.swift
+//  ClearSky
+//
+//  Created by Dylan  on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ClearSkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
