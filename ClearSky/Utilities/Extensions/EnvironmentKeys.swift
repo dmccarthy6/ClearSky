ee51c67494f74933f64a1e29162fd7ed6c811843
Dylan McCarthy <mccarthy.dylan@gmail.com>
2024-10-07 16:22:26 -0400

    refactor: move environment keys to their own file

--- ClearSky/Utilities/Extensions/EnvironmentKeys.swift
+++ ClearSky/Utilities/Extensions/EnvironmentKeys.swift
@@ -1,12 +1,13 @@
 //
-//  LocationManager+EnvironmentKey.swift
+//  EnvironmentKeys.swift
 //  ClearSky
 //
-//  Created by Dylan  on 10/4/24.
+//  Created by Dylan  on 10/7/24.
 //
 
 import SwiftUI
 
 extension EnvironmentValues {
     @Entry var locationManager = LocationManager()
+    @Entry var coordinator = AppCoordinator()
 }
