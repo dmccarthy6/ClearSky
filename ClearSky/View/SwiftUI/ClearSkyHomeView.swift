0fa34556052d8db4083a2bb3cdf9a6bab41ed788
Dylan McCarthy <mccarthy.dylan@gmail.com>
2024-10-08 17:38:28 -0400

    refactor: home view to fetch data more efficiently

--- ClearSky/View/SwiftUI/ClearSkyHomeView.swift
+++ ClearSky/View/SwiftUI/ClearSkyHomeView.swift
@@ -10,30 +10,47 @@ import SwiftUI
 struct ClearSkyHomeView: View {
     @Environment(\.locationManager) var locationManager
     @StateObject var coordinator = AppCoordinator()
-    @State private var city: CityInfo?
-    private let viewModel: HomeViewModel
+    @State private var isLoading = false
+    let viewModel: HomeViewModel
 
     var body: some View {
         NavigationStack(path: $coordinator.routes) {
-            Text("A View")
-                .navigationTitle("Welcome")
-                .navigationDestination(for: AppCoordinator.Routes.self) { route in
-                    switch route {
-                    case .search: Text("Search")
-                    case .weatherInfo:
-                        coordinator.weatherInfoView()
-                    }
+            VStack {
+                if isLoading {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .controlSize(.large)
                 }
+            }
+            .navigationTitle("Home")
+            .navigationDestination(for: AppCoordinator.Routes.self) { route in
+                coordinator.viewFor(route: route)
+            }
         }
         .task {
-            city = await viewModel.location(manager: locationManager,
-                                               database: coordinator.database)
-            coordinator.navigate(to: city == nil ? .search : .weatherInfo)
+            await configureViewAndNavigate()
+        }
+        .onChange(of: locationManager.authStatus) {
+            print("User updated auth status - Home View")
+            Task { @MainActor in
+
+                await configureViewAndNavigate()
+            }
         }
     }
 
-    init(viewModel: HomeViewModel) {
-        self.viewModel = viewModel
+    private func configureViewAndNavigate() async {
+        isLoading = true
+        if locationManager.locationManager.authorizationStatus == .notDetermined {
+            locationManager.requestLocationAuthorizationFromUser()
+            print("Requesting authorization from user")
+            return
+        }
+
+        let cityInfo = await viewModel.cityInfo(manager: locationManager,
+                                                database: coordinator.database)
+        isLoading = false
+        coordinator.navigate(to: cityInfo == nil ? .search : .weatherInfo)
     }
 }
 
