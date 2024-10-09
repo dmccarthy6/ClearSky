1ff6e166f484ccbc4e6bb2ca6d8cfc667282673a
Dylan McCarthy <mccarthy.dylan@gmail.com>
2024-10-09 10:42:54 -0400

    refactor: errors to handle some HTTP and Location errors

--- ClearSky/Utilities/Errors.swift
+++ ClearSky/Utilities/Errors.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
-enum LocationError: Error {
+enum LocationError: Error, Identifiable {
+    var id: Self {
+        self
+    }
+
     case currentLocationFailed
+    case unhandled
+
+    var description: String {
+        return "Unable to retrieve your current location from the system. Try searching for your current city."
+    }
+}
+
+enum HTTPError: Error, Identifiable {
+    var id: Self {
+        self
+    }
+
+    // Decoding Errors (Internal)
+    case typeMismatch
+    case valueNotFound
+    case keyNotFound
+    case dataCorrupted
+    // This case is a catch-all for all other HTTP Errors
+    // for simplicity sake. Would need to fix this.
+    case unhandled
+
+    var description: String {
+        // For simplification just returning a single error string.
+        return "Something went wrong retrieving the weather. Check your internet connection and try again."
+    }
 }
